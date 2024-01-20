b3e18b5c0943fbd9c4b004d68269dd3ce8a0c8d3
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-01-20 22:58:35 +0300

    add new file

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
 
     <div className="cards">
       <Card theme={themeCard.cardGreen} name={data.tariffs[0].name} price={data.tariffs[0].price} speed={data.tariffs[0].speed} comment={data.tariffs[0].comment}></Card>
-      <Card theme={themeCard.cardRed} name={data.tariffs[1].name} price={data.tariffs[1].price} speed={data.tariffs[1].speed} comment={data.tariffs[0].comment}></Card>
-      <Card theme={themeCard.cardYellow} name={data.tariffs[2].name} price={data.tariffs[2].price} speed={data.tariffs[2].speed} comment={data.tariffs[0].comment}></Card>
-      <Card theme={themeCard.cardBlue} name={data.tariffs[3].name} price={data.tariffs[3].price} speed={data.tariffs[3].speed} comment={data.tariffs[0].comment}></Card>
+      <Card theme={themeCard.cardRed} name={data.tariffs[1].name} price={data.tariffs[1].price} speed={data.tariffs[1].speed} comment={data.tariffs[1].comment}></Card>
+      <Card theme={themeCard.cardYellow} name={data.tariffs[2].name} price={data.tariffs[2].price} speed={data.tariffs[2].speed} comment={data.tariffs[2].comment}></Card>
+      <Card theme={themeCard.cardBlue} name={data.tariffs[3].name} price={data.tariffs[3].price} speed={data.tariffs[3].speed} comment={data.tariffs[3].comment}></Card>
 
     </div >
   );
