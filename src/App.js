70906140a8827a312c45a046e4e2d2a475d98f7a
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-01-29 23:10:28 +0300

    add new functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Card from './components/Card';
-import themeCard from '../src/components/themeCard.module.css'
+import themeCard from '../src/components/themeCard.module.css';
+
 
 
 import data from './data.json';
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,24 @@
 import styles from './card.module.css';
+import React, { useState } from 'react';
 
-// export default function Card({ name, price, speed, comment }) {
 
-//     return (
-// <div className={styles.cardWrapper}>
-//     <div className={styles.cardMain}>
-//         <h2 className="tariff-title">{name}</h2>
-//         <h3 className="tariff-subtitle">{price}</h3>
-//     </div>
-//     <h6 className="tariff-speed">{speed}</h6>
-//     <h6 className="comment">{comment}</h6>
-
-//     )
-// }
 
 export default function Card(props) {
 
+    const [active, setActive] = useState(false);
+
+    const handler = () => {
+        setActive(!active);
+    };
+
     return (
-        <div className={styles.cardWrapper}>
+        <div className={`${active ? styles.active : styles.none} ${styles.cardWrapper}`} onClick={handler} >
             <div className={props.theme}>
                 <h2 className="tariff-title">{props.name}</h2>
                 <h3 className="tariff-subtitle">{props.price}</h3>
             </div>
             <h6 className="tariff-speed">{props.speed}</h6>
             <h6 className="comment">{props.comment}</h6>
-        </div>
+        </div >
     )
 }
